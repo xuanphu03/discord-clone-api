948c23eec0329750c6894c011ec2cb719b1ad816
Xuan Phu <xuanphu1024@gmailcom>
2023-12-19 14:31:08 +0700

    feat(master): add logger

--- api/index.ts
+++ api/index.ts
@@ -1,11 +1,22 @@
-import { Hono } from 'hono'
-import { handle } from 'hono/vercel'
+import { Hono } from 'hono';
+import { handle } from 'hono/vercel';
+import { cors } from 'hono/cors';
+import { logger } from 'hono/logger';
 
 export const config = {
   runtime: 'edge',
 }
 
 const app = new Hono().basePath('/api')
+
+app.use('*', logger());
+app.use(
+  '*',
+  cors({
+    origin: ['http://localhost:5173', 'https://discord-web-clone.vercel.app'],
+    credentials: true,
+  })
+);
 
 app.get('/', (c) => c.json({ message: 'Hello Hono!' }));
 app.post('/sign-in', (c) => c.json({ token: '1233121' }));
