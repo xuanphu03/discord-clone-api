267a178b50235754c8097cf3fe821f192ddc5d7e
Xuan Phu <xuanphu1024@gmail.com>
2024-01-30 09:06:05 +0700

    feat(master): add api for orgs/member

--- src/index.ts
+++ src/index.ts
@@ -22,71 +22,7 @@ app.use(
 );
 // app.route('/', authRouter);
 
-app.get('/orgs/:orgID/:channelID/members', c => c.json([
-  {
-    id: 1,
-    displayName: 'Shin',
-    userName: 'shinosuke123',
-    avatar: 'https://th.bing.com/th/id/OIP.GS0ptM4PYsIKvcRmTCoOTgHaEF?w=309&h=180&c=7&r=0&o=5&pid=1.7',
-    backgroundColor: '#2fffff',
-    roles: ['Học viên'],
-    category: {
-      id: 1,
-      name: 'Đà Nẵng',
-    },
-  },
-  {
-    id: 2,
-    displayName: 'XunaFu',
-    userName: 'kuma.xp03',
-    avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQsxcQs95fyxfOtD_tiH5-dedux-NtPJ9HRWg&usqp=CAU',
-    backgroundColor: '#2faf9f',
-    roles: ['Admin', 'F0'],
-    category: {
-      id: 1,
-      name: 'Đà Nẵng',
-    },
-  },
-  {
-    id: 3,
-    displayName: 'Xuna',
-    userName: 'Xunafudev',
-    avatar: 'https://th.bing.com/th/id/OIP.GS0ptM4PYsIKvcRmTCoOTgHaEF?w=309&h=180&c=7&r=0&o=5&pid=1.7',
-    backgroundColor: '#56d6fd',
-    roles: ['Học viên'],
-    category: {
-      id: 1,
-      name: 'Đà Nẵng',
-    },
-  },
-  {
-    id: 4,
-    displayName: 'Bitay',
-    userName: 'trantin03',
-    avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQsxcQs95fyxfOtD_tiH5-dedux-NtPJ9HRWg&usqp=CAU',
-    backgroundColor: '#56d6fd',
-    roles: ['Học viên'],
-    category: {
-      id: 2,
-      name: 'Game',
-    },
-  },
-  {
-    id: 5,
-    displayName: 'ShibaInu',
-    userName: 'akaimaru',
-    avatar:
-      'https://th.bing.com/th?q=HSL-35&w=120&h=120&c=1&rs=1&qlt=90&cb=1&pid=InlineBlock&mkt=en-WW&cc=VN&setlang=vi&adlt=strict&t=1&mw=247',
-    backgroundColor: 'red',
-    roles: ['Học viên'],
-    category: {
-      id: 1,
-      name: 'Đà Nẵng',
-    },
-  },
-]));
-
-
+app.route('orgs', orgsRouter);
 // app.all('*', auth).route('/orgs', orgsRouter);
 // app.all('*', auth).route('/users', usersRouter);
 // app.all('*', auth).route('/channels', channelsRouter);
--- src/modules/orgs/orgs.controller.ts
+++ src/modules/orgs/orgs.controller.ts
@@ -4,26 +4,82 @@ import { OrgsService } from "./orgs.service";
 export const router = new Hono();
 
 router
-  .get('/:orgId', async (c) => {
-    const orgId = c.req.param('orgId');
-
-    const org = await OrgsService.getBy(orgId);
-
-    return c.json({
-      data: org,
-      status: 200,
-    });
-  })
-  .post('/', async (c) => {
-    const user = c.get('user');
-
-    const { name, icon } = await c.req.json();
-
-    const org = await OrgsService.create({
-      userId: user.id,
-      name,
-      icon,
-    });
-
-    return c.json(org);
-  });
+  .get('/:orgID/members', (c) =>
+    c.json([
+      {
+        id: '001',
+        displayName: 'xuna fu',
+        username: 'Xuan Phu',
+        avatar: 'https://th.bing.com/th/id/OIP.GS0ptM4PYsIKvcRmTCoOTgHaEF?w=309&h=180&c=7&r=0&o=5&pid=1.7',
+        memberSince: '2022-01-01',
+        joinedDiscord: '2022-01-01',
+        joinMethod: 'Discord',
+        roles: ["Admin"],
+      },
+    ])
+  )
+  .get('/:orgID/:channelID/members', (c) =>
+    c.json([
+      {
+        id: 1,
+        displayName: 'Shin',
+        userName: 'shinosuke123',
+        avatar: 'https://th.bing.com/th/id/OIP.GS0ptM4PYsIKvcRmTCoOTgHaEF?w=309&h=180&c=7&r=0&o=5&pid=1.7',
+        backgroundColor: '#2fffff',
+        roles: ['Học viên'],
+        category: {
+          id: 1,
+          name: 'Đà Nẵng',
+        },
+      },
+      {
+        id: 2,
+        displayName: 'XunaFu',
+        userName: 'kuma.xp03',
+        avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQsxcQs95fyxfOtD_tiH5-dedux-NtPJ9HRWg&usqp=CAU',
+        backgroundColor: '#2faf9f',
+        roles: ['Admin', 'F0'],
+        category: {
+          id: 1,
+          name: 'Đà Nẵng',
+        },
+      },
+      {
+        id: 3,
+        displayName: 'Xuna',
+        userName: 'Xunafudev',
+        avatar: 'https://th.bing.com/th/id/OIP.GS0ptM4PYsIKvcRmTCoOTgHaEF?w=309&h=180&c=7&r=0&o=5&pid=1.7',
+        backgroundColor: '#56d6fd',
+        roles: ['Học viên'],
+        category: {
+          id: 1,
+          name: 'Đà Nẵng',
+        },
+      },
+      {
+        id: 4,
+        displayName: 'Bitay',
+        userName: 'trantin03',
+        avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQsxcQs95fyxfOtD_tiH5-dedux-NtPJ9HRWg&usqp=CAU',
+        backgroundColor: '#56d6fd',
+        roles: ['Học viên'],
+        category: {
+          id: 2,
+          name: 'Game',
+        },
+      },
+      {
+        id: 5,
+        displayName: 'ShibaInu',
+        userName: 'akaimaru',
+        avatar:
+          'https://th.bing.com/th?q=HSL-35&w=120&h=120&c=1&rs=1&qlt=90&cb=1&pid=InlineBlock&mkt=en-WW&cc=VN&setlang=vi&adlt=strict&t=1&mw=247',
+        backgroundColor: 'red',
+        roles: ['Học viên'],
+        category: {
+          id: 1,
+          name: 'Đà Nẵng',
+        },
+      },
+    ])
+  );;
