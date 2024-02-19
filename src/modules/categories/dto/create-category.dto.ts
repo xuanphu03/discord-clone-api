8a4b33f31f862286e11e980bf80b1ebc7d396040
Xuan Phu <xuanphu1024@gmail.com>
2024-02-19 13:12:45 +0700

    test

--- src/modules/categories/dto/create-category.dto.ts
+++
@@ -1,6 +0,0 @@
-import { z } from 'zod';
-
-export const upsertCategoryDto = z.object({
-  name: z.string().min(1, 'Name is required'),
-  isPrivate: z.boolean().default(false),
-});
--- src/modules/channels/dto/channel.dto.ts
+++
@@ -1,8 +0,0 @@
-import { ChannelType } from '@prisma/client';
-import { z } from 'zod';
-
-export const upsertChannelDto = z.object({
-  name: z.string().min(1, 'Name is required'),
-  isPrivate: z.boolean(),
-  type: z.nativeEnum(ChannelType),
-});
