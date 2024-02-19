eeba2264df01fd6640e2b2d233aaa37bf5439fd7
Xuan Phu <xuanphu1024@gmail.com>
2024-02-19 13:32:49 +0700

    test

--- src/index.ts
+++ src/index.ts
@@ -12,17 +12,13 @@ app.use('*', logger());
 app.use(
   '*',
   cors({
-    origin: ['http://localhost:5173', 'https://discord-web-clone.vercel.app'],
+    origin: ['http://localhost:5173', 'https://discord-clone-api.vercel.app'],
     credentials: true,
   })
 );
 
 app.route('/', authRouter);
 app.route('/orgs', orgsRouter);
-// app.all('*', auth).route('/orgs', orgsRouter);
-// app.all('*', auth).route('/users', usersRouter);
-// app.all('*', auth).route('/channels', channelsRouter);
-// app.all('*', auth).route('/categories', categoriesRouter);
 
 app.notFound((c) => c.json({ status: 404, message: 'Not found' }, 404));
 
