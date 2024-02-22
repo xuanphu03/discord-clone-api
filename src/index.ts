9229ab4dda8f606782ba8ca7b43522a1a32ebf26
Xuan Phu <xuanphu1024@gmail.com>
2024-02-22 17:24:13 +0700

    feat(master): reinstall

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import { Hono } from 'hono';
+import { serve } from '@hono/node-server';
+import { cors } from 'hono/cors';
+import { logger } from 'hono/logger';
+import { router as authRouter } from './modules/auth/auth.controller';
+import { router as orgsRouter } from './modules/orgs/orgs.controller';
+import { errorFilter } from './middlewares/error-fillters';
+import { auth } from './middlewares/auth';
+
+const app = new Hono().basePath('/api');
+
+app.use('*', logger());
+app.use(
+  '*',
+  cors({
+    origin: ['http://localhost:5173', 'https://discord-web-clone.vercel.app'],
+    credentials: true,
+  })
+);
+
+app.route('/', authRouter);
+app.all('*', auth).route('/orgs', orgsRouter);
+
+app.notFound((c) => c.json({ status: 404, message: 'Not found' }, 404));
+
+app.onError(errorFilter);
+
+serve(app, () => {
+  console.log('Server is running on http://localhost:3000');
+});
