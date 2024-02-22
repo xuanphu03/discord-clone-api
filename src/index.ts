a8833b05eec66e6fcd480502b3717c5df50f8c8e
Xuan Phu <xuanphu1024@gmail.com>
2024-02-22 15:35:58 +0700

    feat(master): fix file index

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,12 @@
 import { Hono } from 'hono';
-import { handle } from '@hono/node-server/vercel'
+// import { handle } from '@hono/node-server/vercel'
 import { cors } from 'hono/cors';
-// import { serve } from '@hono/node-server';
+import { serve } from '@hono/node-server';
 import { logger } from 'hono/logger';
 import { router as authRouter } from './modules/auth/auth.controller';
 import { router as orgsRouter } from './modules/orgs/orgs.controller';
 import { errorFilter } from './middlewares/error-fillters';
 import { auth } from './middlewares/auth';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 const app = new Hono().basePath('/api');
 
@@ -29,8 +26,8 @@ app.notFound((c) => c.json({ status: 404, message: 'Not found' }, 404));
 
 app.onError(errorFilter);
 
-// serve(app, () => {
-//   console.log('Server is running on http://localhost:3000');
-// });
+serve(app, () => {
+  console.log('Server is running on http://localhost:3000');
+});
 
-export default handle(app);
+// export default handle(app);
