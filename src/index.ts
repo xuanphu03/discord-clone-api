4e47e7b10fed070d884eaad417a0e4f0d425aee7
Xuan Phu <xuanphu1024@gmail.com>
2024-02-22 16:29:13 +0700

    feat(master):fix contruct index

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import { Hono } from 'hono';
-// import { handle } from '@hono/node-server/vercel'
+import { handle } from '@hono/node-server/vercel'
 import { cors } from 'hono/cors';
-import { serve } from '@hono/node-server';
 import { logger } from 'hono/logger';
 import { router as authRouter } from './modules/auth/auth.controller';
 import { router as orgsRouter } from './modules/orgs/orgs.controller';
@@ -10,14 +9,14 @@ import { auth } from './middlewares/auth';
 
 const app = new Hono().basePath('/api');
 
-app.use('*', logger());
-app.use(
-  '*',
-  cors({
-    origin: ['http://localhost:5173', 'https://discord-web-clone.vercel.app'],
-    credentials: true,
-  })
-);
+// app.use('*', logger());
+// app.use(
+//   '*',
+//   cors({
+//     origin: ['http://localhost:5173', 'https://discord-web-clone.vercel.app'],
+//     credentials: true,
+//   })
+// );
 
 app.route('/', authRouter);
 app.all('*', auth).route('/orgs', orgsRouter);
@@ -26,8 +25,4 @@ app.notFound((c) => c.json({ status: 404, message: 'Not found' }, 404));
 
 app.onError(errorFilter);
 
-serve(app, () => {
-  console.log('Server is running on http://localhost:3000');
-});
-
-// export default handle(app);
+export default handle(app);
